6ac2ae30430fb88c284367060ac4e84258c535e7
noshiro-pf <noshiro.pf@gmail.com>
2026-03-05 04:53:29 +0900

    .

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -7,6 +7,35 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'SynState',
+      favicon: '/favicon/favicon.ico',
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            href: '/synstate/favicon/favicon-32x32.png',
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            href: '/synstate/favicon/favicon-16x16.png',
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+            href: '/synstate/favicon/apple-icon-180x180.png',
+          },
+        },
+      ],
       sidebar: [
         {
           label: 'Getting Started',
